fa85b384c4957624cbcd2e66f8b3ee6e9ff6828e
Aaron Dean <adean30462@gmail.com>
2023-02-15 10:26:30 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var cohortOneStudents = ["Sable", "David", "Joey", "Nick", "Tommy", "Connor", "Charles", "Russ", "Bobby", "Josh", "Nikki", "Matt", "Dale", "Sydney"]
+
+// for(var i = 0; i < moviesArray.length; i++){
+//     console.log(moviesArray[i]);
+//   }
+
+for(var i = 0; i < cohortOneStudents.length; i++){
+     console.log(cohortOneStudents[i]);
+};
+
+var myInterest = ["Food", "BJJ", "Music", "Games", "Movies"];
+
+for(var i = 0; i < myInterest.length; i++){
+    console.log(`One of my interest is: ${myInterest[i]}`);
+};
+  if(myInterest = 1, console.log( "My absolute favorite interest is BJJ"));
